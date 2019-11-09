0676596694e6c288ecf414b47e3beb5b6c419f4a
wjh720 <1040594377@qq.com>
2019-11-09 13:57:18 +0800

    Update grid_world_env.py

--- rllab/envs/grid_world/grid_world_env.py
+++ rllab/envs/grid_world/grid_world_env.py
@@ -26,15 +26,14 @@ from rllab.envs.atari.atari_wrappers import wrap_deepmind, make_atari, get_wrapp
 def get_venv(args):
 	env_type = args.env
 	if env_type == 'pass':
-		env = make_multi_pass_env(env_id, env_type, num_env, seed, args)
+		env = Pass(args, 0)
 	elif env_type == 'island':
-		env = make_m_island_env(env_id, env_type, num_env, seed, args)
+		env = Island(args, 0)
 	elif env_type == 'x_island':
-		env = make_m_x_island_env(env_id, env_type, num_env, seed, args)
+		env = x_Island(args, 0)
 	elif env_type == 'pushball':
-		env = make_m_pushball_env(env_id, env_type, num_env, seed, args)
-	venv = TfEnv(env)
-	return venv
+		env = PushBall(args, 0)
+	return env
 
 
 class Grid_World_Env(Env, Serializable):
@@ -66,33 +65,14 @@ class Grid_World_Env(Env, Serializable):
 		return self._horizon
 
 	def reset(self):
-		if self._force_reset and self.monitoring:
-			from gym.wrappers.monitoring import Monitor
-			assert isinstance(self.env, Monitor)
-			recorder = self.env.stats_recorder
-			if recorder is not None:
-				recorder.done = True
 		return self.env.reset()
 
 	def step(self, action):
 		next_obs, reward, done, info = self.env.step(action)
 		return Step(next_obs, reward, done, **info)
 
-	def render(self, mode='human'):
-		return self.env.render(mode)
+	def render(self):
+		pass
 
 	def terminate(self):
-		if self.monitoring:
-			self.env._close()
-			if self._log_dir is not None:
-				print("""
-	***************************
-	Training finished! You can upload results to OpenAI Gym by running the following command:
-	python scripts/submit_gym.py %s
-	***************************
-				""" % self._log_dir)
-
-	def get_original_frames(self):
-		if not self.save_original_frames:
-			return None
-		return self.original_frame_saver.get_frames()
+		pass
