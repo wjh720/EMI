92334f7ae2a28cc47eb8cf3dc59ce80064117007
wjh720 <1040594377@qq.com>
2019-11-10 18:42:37 +0800

    Update pushball_environment.py

--- rllab/envs/grid_world/pushball_environment.py
+++ rllab/envs/grid_world/pushball_environment.py
@@ -29,7 +29,7 @@ class PushBall:
 		# return np.array([self.size // 2, self.size // 2])
 		# return np.array(np.random.randint(self.size // 4, self.size // 4 * 3, [self.n_dim]))
 		# return np.array(np.random.randint(1, self.size - 1, [self.n_dim]))
-		return np.array(np.random.randint(4, 11, [self.n_dim]))
+		return np.array(np.random.randint(4, 12, [self.n_dim]))
 
 	def random_start_n(self):
 		ball_list = []
