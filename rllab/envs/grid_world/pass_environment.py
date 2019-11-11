7da38a6dfa540ca2da42252b091e67021986cda1
wjh720 <1040594377@qq.com>
2019-11-11 14:21:29 +0800

    Update pass_environment.py

--- rllab/envs/grid_world/pass_environment.py
+++ rllab/envs/grid_world/pass_environment.py
@@ -22,7 +22,7 @@ class Pass:
         res = []
         for item in self.action_space_x:
             res.append(action_n % item)
-            action_n /= int(item)
+            action_n //= item
         return res
 
     def initialization(self, args):
